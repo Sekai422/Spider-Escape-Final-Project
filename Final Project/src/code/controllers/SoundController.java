87a4b3780e9c300d3516149dee903c5128147b60
Ali Siddiqui <fudgecoated@gmail.com>
2018-04-13 16:26:07 -0600

    comment music

--- Final Project/src/code/controllers/SoundController.java
+++ Final Project/src/code/controllers/SoundController.java
@@ -14,12 +14,19 @@ public class SoundController {
     Media damageSound = new Media(new File("media/hit.wav").toURI().toString());
     MediaPlayer musicPlayer = new MediaPlayer(music);
 
+    /**
+     * A MediaPlayer object using a Media object is created and played.
+     * @param sound this is the JavaFX media file that will be played
+     */
     public void playMedia (Media sound) {
 
         MediaPlayer mediaPlayer = new MediaPlayer(sound);
         mediaPlayer.play();
     }
 
+    /**
+     * The game music is played and repeated using the music Media object.
+     */
     public void playMusic() {
 
         musicPlayer.setOnEndOfMedia(new Runnable() {
@@ -30,10 +37,17 @@ public class SoundController {
         musicPlayer.play();
     }
 
+    /**
+     * The MediaPlayer object for music is paused.
+     */
     public void pauseMusic() {
         musicPlayer.pause();
     }
 
+    /**
+     * Used to control MediaPlayer object for music in pause menu by returning the musicPlayer from SoundController class.
+     * @return musicPlayer MediaPlayer object for music.
+     */
 	public MediaPlayer getMusicPlayer() {
 		return musicPlayer;
 	}
